b2e87bcb8e353570b14d49643810f1e237bdd403
Ringo <m13>
2021-03-12 11:20:14 +0100

    added Gradient View

--- NorthLib/Pdf/PdfOverviewCollectionVC.swift
+++ NorthLib/Pdf/PdfOverviewCollectionVC.swift
@@ -61,6 +61,14 @@ public class PdfOverviewCollectionVC : UICollectionViewController, CanRotate{
       pin(cv.top, to: cvsv.top)
       pin(cv.left, to: cvsv.leftGuide())
       pin(cv.right, to: cvsv.rightGuide())
+      /* TODO@Ringo: Demo top Gradient, need Design Decission
+      let topGradient = VerticalGradientView()
+      topGradient.pinHeight(UIWindow.topInset)
+      cvsv.addSubview(topGradient)
+      pin(topGradient.left, to: cvsv.leftGuide())
+      pin(topGradient.right, to: cvsv.rightGuide())
+      pin(topGradient.top, to: cvsv.top)
+      */
     }
   }
   
