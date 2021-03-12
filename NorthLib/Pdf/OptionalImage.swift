7e7e0417ddfc992f6328bc06bf780ecc1899a963
Ringo <m13>
2021-03-12 17:28:27 +0100

    removed currently unused function

--- NorthLib/Pdf/OptionalImage.swift
+++ NorthLib/Pdf/OptionalImage.swift
@@ -24,7 +24,7 @@ public protocol ZoomedPdfImageSpec : OptionalImage, DoesLog {
   var size : CGSize { get }
   var fullScreenPageHeight : CGFloat? { get }
   
-  func resetToSingleScaleIfNeeded()
+//  func resetToSingleScaleIfNeeded()
   func renderImageWithNextScale(finishedCallback: ((Bool) -> ())?)
   func renderFullscreenImageIfNeeded(finishedCallback: ((Bool) -> ())?)
   func renderImageWithScale(scale: CGFloat, finishedCallback: ((Bool) -> ())?)
