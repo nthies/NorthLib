fe2a380fdc7400ca33bbdf4f601727d889be5c7c
Ringo <i17>
2021-03-19 16:10:54 +0100

    Download StatusButton

---
+++ NorthLib/Views/DownloadStatusButton.swift
@@ -0,0 +1,240 @@
+//
+//  DownloadStatusButton.swift
+//  Test App UIKit
+//
+//  Created by Ringo Müller on 17.03.21.
+//
+
+import Foundation
+import UIKit
+import NorthLib
+
+public enum DownloadStatusButtonState { case notStarted, process, done }
+
+public class DownloadStatusButton : UIButton {
+  private var lastBounds:CGRect?
+  private lazy var progressCircle = ProgressCircle()
+  
+  private let iconPadding:CGFloat = 3.0
+  private let verticalPadding:CGFloat = 6.0
+
+  public var startHandler : (()->())?
+  public var stopHandler : (()->())?
+  public var downloadState: DownloadStatusButtonState = .notStarted {
+    didSet{
+      switch downloadState {
+        case .notStarted:
+          percent = 0.0
+        case .done:
+          percent = 1.0
+        default:
+          break
+      }
+      self.update()
+    }
+  }
+  
+  public var percent:Float = 0.0 {
+    didSet {
+      if downloadState == .process, oldValue != percent {
+        progressCircle.progress = percent
+        if percent == 1.0 {
+          onMainAfter(2.0) { [weak self] in
+            self?.downloadState = .done
+            self?.startHandler = nil
+            self?.stopHandler = nil
+            self?.update()
+          }
+        }
+      }
+    }
+  }
+  
+  public var cloudImage : UIImage? = UIImage(name: "icloud.and.arrow.down")
+  
+  public var buttonImage : UIImage? {
+    didSet{
+      if oldValue != buttonImage {
+        self.setImage(buttonImage, for: .normal)
+      }
+    }
+  }
+  
+  func update() {
+    let offset = progressCircle.bounds.width + 2*iconPadding
+    switch downloadState {
+      case .notStarted:
+        buttonImage = cloudImage
+        progressCircle.isHidden = true
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: offset,
+                                            bottom: 0, right: 0)
+      case .process:
+        buttonImage = nil
+        progressCircle.isHidden = false
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: offset,
+                                            bottom: 0, right: offset)
+        //Center Label
+      case .done:
+        buttonImage = nil
+        progressCircle.isHidden = true
+        self.titleEdgeInsets = .zero
+    }
+  }
+  
+  public override func tintColorDidChange() {
+    super.tintColorDidChange()
+    self.progressCircle.color = self.tintColor
+  }
+  
+  public override func willMove(toSuperview newSuperview: UIView?) {
+    if newSuperview != nil { setup(); update() }
+    super.willMove(toSuperview: newSuperview)
+  }
+
+  
+  func setup() {
+    self.addTarget(self,
+                   action: #selector(self.handleButtonPress),
+                   for: .touchUpInside)
+    self.layer.addSublayer(progressCircle)
+    if let imageView = self.imageView {
+      imageView.centerY()
+      pin(imageView.right, to: self.right)
+    }
+    self.semanticContentAttribute = .forceRightToLeft
+  }
+
+  @objc func handleButtonPress(){
+    if downloadState == .notStarted, let handler = startHandler {
+      downloadState = .process
+      percent = 0.0
+      handler()
+    }
+    else if downloadState == .process,
+            percent < 1.0,
+            let handler = stopHandler {
+      downloadState = .notStarted
+      percent = 0.0
+      handler()
+    }
+  }
+      
+  private func updateBoundsIfNeeded(){
+    if lastBounds == self.bounds { return }
+    lastBounds = self.bounds
+    
+    let diam = self.bounds.height - 2*verticalPadding
+    let offsetRight = self.bounds.width - diam - iconPadding
+    progressCircle.frame =  CGRect(x: offsetRight,
+                                   y: verticalPadding,
+                                   width: diam,
+                                   height: diam)
+  }
+  
+  public override func layoutSubviews() {
+    updateBoundsIfNeeded()
+    super.layoutSubviews()
+  }
+}
+
+class ProgressCircle: CALayer {
+  
+  public var progress: Float = 0.0 {
+    didSet{
+      if let tv = self.animation.toValue as? Float, progress - tv < 0.1 { return }
+      self.progressCircle.strokeColor = color.cgColor
+      onMain { [weak self] in
+        guard let self = self else { return }
+        self.animation.fromValue = oldValue
+        self.animation.toValue = self.progress
+        if self.progressCircle.animation(forKey: "ani1") != nil {
+          self.progressCircle.add(self.animation, forKey: "ani2")
+          self.progressCircle.removeAnimation(forKey: "ani1")
+        } else {
+          self.progressCircle.removeAnimation(forKey: "ani2")
+          self.progressCircle.add(self.animation, forKey: "ani1")
+        }
+        self.stopIcon.backgroundColor = self.color.cgColor
+      }
+    }
+  }
+  
+  /// Properties
+  public var color:UIColor = UIColor.red {
+    didSet{
+      let col = progress > 0.0 ? color.cgColor : UIColor.clear.cgColor
+      progressCircle.strokeColor = col
+      stopIcon.backgroundColor = col
+    }
+  }
+  
+  public var trackColor:UIColor = UIColor.gray {
+    didSet{
+      progressTrackCircle.strokeColor = trackColor.cgColor
+    }
+  }
+  
+  /// UI Components
+  private lazy var progressCircle : CAShapeLayer = {
+    let circle = CAShapeLayer ()
+    circle.strokeColor = UIColor.clear.cgColor
+    circle.fillColor = UIColor.clear.cgColor
+    circle.lineWidth = 1.5
+    circle.strokeStart = 0.0
+    circle.strokeEnd = 0.0
+    return circle
+  }()
+  
+  private lazy var progressTrackCircle : CAShapeLayer = {
+    let circle = CAShapeLayer ()
+    circle.strokeColor = trackColor.cgColor
+    circle.fillColor = UIColor.clear.cgColor
+    circle.lineWidth = 1.5
+    return circle
+  }()
+  
+  private lazy var stopIcon = CALayer ()
+  
+  private lazy var animation : CABasicAnimation = {
+    let animation = CABasicAnimation(keyPath: "strokeEnd")
+    animation.duration = 0.3
+    animation.isAdditive = true
+    animation.fillMode = CAMediaTimingFillMode.forwards
+    animation.isRemovedOnCompletion = false
+    return animation
+  }()
+  
+  override var frame: CGRect {
+    didSet {
+      updateComponentsIfNeeded()
+    }
+  }
+  
+  private var lastBounds:CGRect?
+  
+  private func updateComponentsIfNeeded(){
+    if lastBounds == self.bounds { return }
+    addSublayerIfNeeded()
+    lastBounds = self.bounds
+    //Layout Circle
+    let diam = self.bounds.height
+    let rect = CGRect(origin: .zero, size: CGSize(width: diam, height: diam))
+    let circlePath = UIBezierPath(roundedRect: rect, cornerRadius: diam/2)
+    progressTrackCircle.path = circlePath.cgPath
+    progressCircle.path = circlePath.cgPath
+    //Layout square in Circle
+    let squareSize:CGFloat = self.bounds.height/5
+    stopIcon.frame = CGRect(x: self.bounds.width - diam/2 - squareSize/2,
+                            y: diam/2 - squareSize/2,
+                            width: squareSize,
+                            height: squareSize)
+    stopIcon.backgroundColor = trackColor.cgColor
+  }
+  
+  private func addSublayerIfNeeded(){
+    if progressCircle.superlayer != nil { return }
+    self.addSublayer(progressTrackCircle)
+    self.addSublayer(progressCircle)
+    self.addSublayer(stopIcon)
+  }
+}
