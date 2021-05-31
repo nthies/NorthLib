7fd3b33a7dc6d168acbdad55fc882ffcda6ddffd
Ringo <i17>
2021-05-31 16:11:43 +0200

    Helper: show/hide animated

--- NorthLib/Extensions/ViewExtensions.swift
+++ NorthLib/Extensions/ViewExtensions.swift
@@ -62,6 +62,35 @@ public extension UIView {
   }
 }
 
+/// A UIView extension to show/hide views animated
+public extension UIView {
+  func showAnimated(completion: (()->())? = nil){
+    if isHidden == false { return }
+    onMain { [weak self] in
+      self?.alpha = 0.0
+      self?.isHidden = false
+      UIView.animate(withDuration: 0.6) {[weak self] in
+        self?.alpha = 1.0
+      } completion: {_ in
+        completion?()
+      }
+    }
+  }
+  
+  func hideAnimated(completion: (()->())? = nil){
+    if isHidden == true { return }
+    onMain { [weak self] in
+      UIView.animate(withDuration: 0.6) {[weak self] in
+        self?.alpha = 0.0
+      } completion: { [weak self] _ in
+        self?.isHidden = true
+        self?.alpha = 1.0
+        completion?()
+      }
+    }
+  }
+}
+
 
 // Layout anchors and corresponding views:
 public struct LayoutAnchorX {
