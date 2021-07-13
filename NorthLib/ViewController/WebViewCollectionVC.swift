c544c3a4f431d925be7812cc400affa7a1357ba5
Norbert Thies <norbert@taz.de>
2021-07-13 14:39:57 +0200

    All closure callbacks now use @Callback wrappers

--- NorthLib/ViewController/WebViewCollectionVC.swift
+++ NorthLib/ViewController/WebViewCollectionVC.swift
@@ -12,65 +12,67 @@ import WebKit
 public protocol WebViewUrl {
   var url: URL { get }
   var isAvailable: Bool { get }
-  func whenAvailable(closure: @escaping ()->())
+  var whenAvailable: Callback<Void>.Store { get }
   func waitingView() -> UIView?
 }
 
 /// An optional WebView using a "waiting view" as long as the web contents is not available
-struct OptionalWebView: OptionalView, DoesLog {
-  
-  var url: WebViewUrl
+class OptionalWebView: OptionalView, DoesLog {
+    
+  var url: WebViewUrl { didSet { urlChanged() } }
   var webView: WebView?
-  var waitingView: UIView?
-  
-  var isAvailable: Bool { return url.isAvailable }
-  func whenAvailable(closure: @escaping () -> ()) { url.whenAvailable(closure: closure) }
   var mainView: UIView? { return webView }
-  func loadView() { if isAvailable { webView?.load(url: url.url) } }
+  var waitingView: UIView? { url.waitingView() }
+
+  @SingleCallback
+  public var whenAvailable: Callback<Void>.Store
   
-  fileprivate mutating func createWebView(vc: WebViewCollectionVC) {
+  var isAvailable = false
+  var willBeAvailable: Bool { url.isAvailable }
+  func loadView() {
+    if url.isAvailable { webView?.load(url: url.url) }
+  }
+  
+  fileprivate func urlChanged() {
+    isAvailable = false
+    if let webView = self.webView { webView.stopLoading() }
+    else { createWebView() }
+    if url.isAvailable { loadView() }
+    else {
+      url.whenAvailable { [weak self] in self?.loadView() }
+    }
+  }
+  
+  fileprivate func createWebView() {
     self.webView = WebView(frame: .zero)
     guard let webView = self.webView else { return }
     webView.isOpaque = false
     webView.backgroundColor = UIColor.clear
     webView.scrollView.backgroundColor = UIColor.clear
-    vc.addWebViewClosures(webView: webView)
     webView.allowsBackForwardNavigationGestures = false
     webView.scrollView.isDirectionalLockEnabled = true
     webView.scrollView.showsHorizontalScrollIndicator = false
-    webView.baseDir = vc.baseDir
     webView.minScrollRatio = 0.01
-    webView.whenScrolled { [weak vc] ratio in vc?.didScroll(ratio: ratio) }
-    if let closure = vc.atEndOfContentClosure {
-      webView.atEndOfContent { isAtEnd in closure(isAtEnd) }
+    webView.whenLoaded { [weak self] _ in
+      self?.isAvailable = true
+      self?.$whenAvailable.notify(sender: self)
     }
   }
 
-  init(vc: WebViewCollectionVC, url: WebViewUrl) {
+  init(url: WebViewUrl) {
     self.url = url
-    self.waitingView = url.waitingView()
-    createWebView(vc: vc)
+    urlChanged()
   }
-  
-  @discardableResult
-  mutating func update(vc: WebViewCollectionVC, url: WebViewUrl) -> OptionalWebView {
-    debug("updating WebView")
-    self.url = url
-    self.waitingView = url.waitingView()
-    webView?.stopLoading()
-//    createWebView(vc: vc)
-    return self
-  }
-    
+      
 } // OptionalWebView
 
 /// A very simple file based WebViewUrl
 public struct FileUrl: WebViewUrl {
   public var url: URL
-  public var path: String { return url.path }
-  public var isAvailable: Bool { return File(path).exists }
-  public func whenAvailable(closure: ()->()) {}
-  public func waitingView() -> UIView? { return nil }
+  public var path: String { url.path }
+  public var isAvailable: Bool { File(path).exists }
+  public var whenAvailable: Callback<Void>.Store { {_ in} }
+  public func waitingView() -> UIView? { nil }
   public init(path: String) { self.url = URL(fileURLWithPath: path) }
 }
 
@@ -94,50 +96,27 @@ open class WebViewCollectionVC: PageCollectionVC {
   public var currentWebView: WebView? { return currentView?.activeView as? WebView }
   public var indicatorStyle:  UIScrollView.IndicatorStyle = .default
 
-  // The closure to call when loading completed
-  private var _whenLoaded: (()->())?
-  
-  /// Define closure to call when content is loaded
-  public func whenLoaded(_ closure: @escaping ()->()) {
-    _whenLoaded = closure
-  }
+  // The closures to call when loading is completed
+  @Callback
+  public var whenLoaded: Callback<Void>.Store
 
-  // The closure to call when link is pressed
-  private var _whenLinkPressed: ((URL?,URL?)->())?
+  // The closures to call when a link is pressed
+  @Callback<(URL?,URL?)>
+  public var whenLinkPressed: Callback<(URL?,URL?)>.Store
   
-  /// Define closure to call when link is pressed
-  public func whenLinkPressed( _ closure: @escaping (URL?,URL?)->() ) {
-    _whenLinkPressed = closure
-  }
+  // The closures to call when the webview has been scrolled more than 5%
+  @Callback<CGFloat>
+  public var whenScrolled: Callback<CGFloat>.Store
   
-  // The closure to call when the webview has been scrolled more than 5%
-  private var _whenScrolled: ((CGFloat)->())?
-  
-  /// Define closure to call when more than 5% has b een scrolled
-  public func whenScrolled(_ closure: ((CGFloat)->())?) {
-    _whenScrolled = closure
-  }
-  
-  // End of content closure
-  var atEndOfContentClosure: ((Bool)->())?
-
-  /// Define closure to call when the end of the web content will become 
-  /// visible
-  public func atEndOfContent(closure: @escaping (Bool)->()) {
-    atEndOfContentClosure = closure
-  }
+  // End of content closures
+  @Callback<Bool>
+  public var atEndOfContent: Callback<Bool>.Store
   
   /// reload contents of current WebView
   open func reload() {
     if let wv = currentWebView { wv.reload() }
   }
-  
-  /// Scroll of WebView detected
-  public func didScroll(ratio: CGFloat) { 
-    guard let closure = _whenScrolled else { return }
-    closure(ratio)
-  }
-  
+    
   public func displayUrls(urls: [WebViewUrl]? = nil) {
     if let urls = urls { self.urls = urls }
     self.count = self.urls.count
@@ -174,7 +153,6 @@ open class WebViewCollectionVC: PageCollectionVC {
   var optionalWebViews:[OptionalWebView] = []
   
   open func reloadAllWebViews(){
-//    print("Reloading #\(optionalWebViews.count) Webviews")
     optionalWebViews.forEach{
       $0.webView?.reload()
       $0.webView?.scrollView.indicatorStyle = indicatorStyle
@@ -186,37 +164,47 @@ open class WebViewCollectionVC: PageCollectionVC {
     self.view.backgroundColor = UIColor.white
     inset = 0
     viewProvider { [weak self] (index, oview) in
-      guard let this = self else { return UIView() }
-      if var ov = oview as? OptionalWebView {
-        ov.webView?.scrollView.indicatorStyle = self?.indicatorStyle ?? .default
-        return ov.update(vc: this, url: this.urls[index])
+      guard let self = self else { return UIView() }
+      if let ov = oview as? OptionalWebView {
+        ov.webView?.scrollView.indicatorStyle = self.indicatorStyle
+        ov.url = self.urls[index]
+        return ov
       }
       else { 
-        let owv = OptionalWebView(vc: this, url: this.urls[index]) 
-        self?.optionalWebViews.append(owv)
-        if let bridge = this.bridge { 
+        let owv = OptionalWebView(url: self.urls[index])
+        self.initWebView(oView: owv)
+        self.optionalWebViews.append(owv)
+        if let bridge = self.bridge {
           owv.webView?.addBridge(bridge)
-          owv.webView?.scrollView.indicatorStyle = self?.indicatorStyle ?? .default
-          if this.isBridgeLogging { owv.webView?.log2bridge(bridge) }
+          owv.webView?.scrollView.indicatorStyle = self.indicatorStyle
+          if self.isBridgeLogging { owv.webView?.log2bridge(bridge) }
         }
         return owv
       }
     }
   }
   
-  public func addWebViewClosures(webView: WebView) {
+  func initWebView(oView: OptionalWebView) {
+    guard let webView = oView.webView else { return }
+    webView.baseDir = baseDir
     webView.whenLoadError { [weak self] err in
       guard let self = self else { return }
       self.error("WebView Load Error on \"\(webView.originalUrl?.lastPathComponent ?? "[undefined URL]")\":\n  \(err.description)")
     }
     webView.whenLinkPressed { [weak self] arg in
-      guard let self = self else { return }
-      self._whenLinkPressed?(arg.from, arg.to)
-      self.onPageChange()
+      self?.$whenLinkPressed.notify(sender: self, content: arg)
+      self?.onPageChange()
     }
     webView.whenLoaded { [weak self] _ in
-      guard let self = self else { return }
-      self._whenLoaded?()
+      self?.$whenLoaded.notify(sender: self)
+    }
+    webView.whenScrolled { [weak self] ratio in
+      self?.$whenScrolled.notify(sender: self, content: ratio)
+    }
+    if $atEndOfContent.count > 0 {
+      webView.atEndOfContent { [weak self] isAtEnd in
+        self?.$atEndOfContent.notify(sender: self, content: isAtEnd)
+      }
     }
   }
     
