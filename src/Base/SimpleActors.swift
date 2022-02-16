9d189d49467f4378e4f229841a67cb41a900e75e
Norbert Thies <norbert@taz.de>
2022-02-16 13:29:49 +0100

    Added the very simple actors Sync and Counter.

--- src/Base/SimpleActors.swift
+++ src/Base/SimpleActors.swift
@@ -1,14 +1,20 @@
 //
-//  Counter.swift
+//  SimpleActors.swift
 //
 //  Created by Norbert Thies on 14.01.22.
 //
 
 /**
+ * Sync is a trivial actor just offering the method _sync_ via the Synchronizable
+ * protocol.
+ */
+public actor Sync: Synchronizable {}
+
+/**
  * The Counter actor offers methods to synchronize access to an Int counter
  * and to offer a method 'sync' to synchronize closures.
  */
-public actor Counter {
+public actor Counter: Synchronizable {
   
   /// The Int counter
   public var value: Int = 0
@@ -16,11 +22,8 @@ public actor Counter {
   /// Increment operation returning the incremented value
   public func inc() -> Int { value += 1; return value }
   
-  /// Synchronize with other closures performed on this actor
-  @discardableResult
-  func sync<Result>(_ closure: () throws -> Result) rethrows -> Result {
-    try closure()
-  }
+  /// Decrement operation returning the decremented value
+  public func dec() -> Int { value -= 1; return value }
   
   /// Initialize with optional start value (by default 0)
   public init(_ val: Int = 0) { value = val }
