3bf07a3227d79b920697b35bb945caaa6984b0aa
Norbert Thies <norbert@taz.de>
2022-02-16 13:40:43 +0100

    Converted the ZipStream class from Objective-C to Swift.

--- src/Base/ZipFile.swift
+++ src/Base/ZipFile.swift
@@ -1,12 +1,44 @@
 //
 //  ZipFile.swift
-//  NorthLib
 //
 //  Created by Norbert Thies on 14.07.21.
 //  Copyright © 2021 Norbert Thies. All rights reserved.
 //
 
-import Foundation
+import NorthLowLevel
+
+/// A class to handle zip streams
+open class ZipStream {
+  
+  /// The C++ reader class
+  private var reader: UnsafeMutableRawPointer?
+  
+  // The closure to call upon file found
+  private var onFileClosure: ((String, Memory)->())?
+  
+  /// Define the closure to call upon file found
+  public func onFile(closure: @escaping (String, Memory)->()) {
+    onFileClosure = closure
+  }
+  
+  public init() {
+    reader = zip_stream_init(voidptr(obj: self)) { (context,name,data,length) in
+      let ctx: ZipStream = voidptr(ptr: context!)
+      let fname = String(validatingUTF8: name!)!
+      ctx.onFileClosure?(fname, Memory(ptr: data, length: Int(length)))
+    }
+  }
+  
+  deinit { zip_stream_release(reader) }
+  
+  /// Scan chunk of data for embedded files
+  public func scanData(mem: Memory, length: Int) throws {
+    if zip_stream_scan(reader, mem.ptr, Int32(length)) < 0 {
+      throw String(validatingUTF8: zip_stream_lasterr(reader))!
+    }
+  }
+  
+} // class ZipStream
 
 /// A class for handling zip-packed files
 open class ZipFile {
@@ -21,19 +53,18 @@ open class ZipFile {
   }
   
   /// Unpack to given directory
-  public func unpack(toDir dir: String) {
-    zipStream.onFile { (name, data) in
-      guard let name = name, let data = data else { return }
+  public func unpack(toDir dir: String) throws {
+    zipStream.onFile { (name, mem) in
       let path = "\(dir)/\(name)"
       Dir(File.dirname(path)).create()
-      File(path).data = data
+      File(path).mem = mem
     }
-    File.open(path: zipFile, mode: "r") { [weak self] file in
+    try File.open(path: zipFile, mode: "r") { [weak self] file in
       guard let self = self else { return }
-      while let data = file.read() {
-        self.zipStream.scanData(data)
+      while let data = file.read(nbytes: 20*1024) {
+        try self.zipStream.scanData(mem: data, length: data.length)
       }
     }
   }
   
-}
+}  // class ZipFile
