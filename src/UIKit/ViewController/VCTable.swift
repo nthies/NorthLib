9cb4996ac5c755badaf24928f4b94d0a3eaab1ea
Norbert Thies <norbert@taz.de>
2022-03-15 13:51:55 +0100

    Added VCTableDelegate protocol to support SplitView navigation

--- src/UIKit/ViewController/VCTable.swift
+++ src/UIKit/ViewController/VCTable.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+/// A ptotocol used by VCTable to show a view controller
+public protocol VCTableDelegate {
+  func showVC(vc: VCDescription) 
+}
+
+/// By default a view controller is pushed onto the navigation stack
+public extension VCTableDelegate where Self: UIViewController {
+  func showVC(vc: VCDescription)  {
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+}
+
 /// A view controller description
 public protocol VCDescription where Self: UIViewController {
   /// Name/Title of view controller
@@ -24,7 +36,6 @@ public protocol VCDescription where Self: UIViewController {
 }
 
 public extension VCDescription {
-  var title: String? { "\(self)" }
   static var title: String { "\(self)" }
   static var description: String { "" }
   static var keepAlive: Bool { false }
@@ -35,17 +46,18 @@ public extension VCDescription {
 open class VCCell: UITableViewCell {
   override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-    detailTextLabel?.numberOfLines = 0
+    detailTextLabel?.numberOfLines = 2
+    detailTextLabel?.adjustsFontSizeToFitWidth = true
   }
   required public init?(coder: NSCoder) { super.init(coder: coder) }
 }
 
 open class VCTable: UIViewController, UITableViewDelegate, 
-                    UITableViewDataSource {
+                    UITableViewDataSource, VCTableDelegate {
   public class VC {
     var vcd: VCDescription.Type
-    var vc: UIViewController?
-    init(vcd: VCDescription.Type, vc: UIViewController? = nil) 
+    var vc: VCDescription?
+    init(vcd: VCDescription.Type, vc: VCDescription? = nil) 
       { self.vcd = vcd; self.vc = vc }
   }
   
@@ -70,10 +82,11 @@ open class VCTable: UIViewController, UITableViewDelegate,
   
   /// Start view controller
   private func start(vcd: VC) {
-    var vc: UIViewController
+    var vc: VCDescription
     if vcd.vc != nil { vc = vcd.vc! }
     else { vc = vcd.vcd.init(nibName: nil, bundle: nil) }
     vcd.vc = vc
+    vc.title = vcd.vcd.title
   }
 
   /// Add a view controller to the table
@@ -91,8 +104,19 @@ open class VCTable: UIViewController, UITableViewDelegate,
   /// Currently displaying menu?
   private var inMenu = false
   
-  /// Set status bar to light mode
-  open override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+  /// Delegate showing view controllers
+  public var delegate: VCTableDelegate!
+  
+  /// Initialize with optional delegate
+  public init(delegate: VCTableDelegate? = nil) {
+    self.delegate = delegate
+    super.init(nibName: nil, bundle: nil)
+    if delegate == nil { self.delegate = self }
+  }
+  
+  required public init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
   
   /// Produce screenshot of currently visible VC
   /// Attention: Info.plist must contain a String entry for
@@ -128,10 +152,8 @@ open class VCTable: UIViewController, UITableViewDelegate,
     win?.addGestureRecognizer(recog)
   }
   
-  /// Destruct previously created VCs
-  override open func viewWillAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    self.navigationController?.setNavigationBarHidden(true, animated: false)
+  /// Destruct previously created VC
+  func destructVC() {
     if let selected = selected {
       let vcd = vcDescriptions[selected]
       if !vcd.vcd.keepAlive { vcd.vc = nil }
@@ -139,6 +161,12 @@ open class VCTable: UIViewController, UITableViewDelegate,
       vcTable.deselectRow(at: indexPath, animated: false)
       self.selected = nil
     }
+  }
+  
+  /// Destruct previously created VC in case of single VC on window
+  override open func viewWillAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    self.navigationController?.setNavigationBarHidden(true, animated: false)
   }
   
   /// Create table view and pin it to the safe margins
@@ -200,12 +228,12 @@ open class VCTable: UIViewController, UITableViewDelegate,
   // MARK: - UITableViewDelegate methods
   
   public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    // destruct previously created VC
+    destructVC()
     self.selected = indexPath.row
     let vcd = vcDescriptions[self.selected!]
     start(vcd: vcd)
-    if let vc = vcd.vc {
-      self.navigationController?.pushViewController(vc, animated: true)
-    }
+    if let vc = vcd.vc { self.delegate.showVC(vc: vc) }
   }
   
 }
