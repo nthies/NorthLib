0876331220e1fc148e21aade93c6f30724dd2082
Ringo <i17>
2023-02-16 11:12:22 +0100

    CarouselFlowLayout changed access control to open for inheritance

--- src/UIKit/Views/CarouselView.swift
+++ src/UIKit/Views/CarouselView.swift
@@ -11,7 +11,7 @@ import UIKit
  A CarouselFlowLayout models a horizontal cell layout with a scaling effect
  to have the central cell enlarged.
  */
-public class CarouselFlowLayout: UICollectionViewFlowLayout, DoesLog {
+open class CarouselFlowLayout: UICollectionViewFlowLayout, DoesLog {
   
   /// The maximum scale to use when increasing the size of the central cell
   var maxScale: CGFloat = 1.3
