7aeb53e7bed35e181f1bbd443cb6c96e141fc919
Ringo <i17>
2023-06-20 00:29:10 +0200

    Helper for groupedContextMenu and smooth image preview appearance also for buttonView<UIImageView>

--- src/UIKit/Views/ContextMenu.swift
+++ src/UIKit/Views/ContextMenu.swift
@@ -42,20 +42,69 @@ open class ContextMenu: NSObject, UIContextMenuInteractionDelegate {
     }
   }
   
-  /// Add an additional menu item
-  public func addMenuItem(title: String, icon: String, 
-                          closure: @escaping (Any?)->()) {
-    menu += (title: title, icon: icon, closure: closure)
-  }
-  
-  fileprivate func createContextMenu() -> UIMenu {
+  fileprivate func createFlatContextMenu() -> UIMenu {
     let menuItems = menu.map { m in
       UIAction(title: m.title,
                image: UIImage(name: m.icon) ?? UIImage(named: m.icon)) { [weak self] _ in
-        m.closure(self?.argument) 
+        m.closure(self?.argument)
       }
     }
     return UIMenu(title: "", children: menuItems)
+  }
+
+  
+  fileprivate func createContextMenu() -> UIMenu {
+    if groupedMenu.count > 0 {
+      return createGroupedContextMenu()
+    }
+    return createFlatContextMenu()
+  }
+  
+  /// Define the menu to display on long touch
+  public var groupedMenu: [(title: String, icon: String, group: Int, closure: (Any?)->())] = [] {
+    willSet {
+      if menu.count == 0 {
+        view.isUserInteractionEnabled = true
+        view.addInteraction(UIContextMenuInteraction(delegate: self))
+      }
+    }
+  }
+  
+  fileprivate func createGroupedContextMenu() -> UIMenu {
+    var itms: [Int:[UIAction]] = [:]
+    for m in groupedMenu {
+      var subItems = itms[m.group] ?? []
+      subItems.append(UIAction(title: m.title,
+                         image: UIImage(name: m.icon)
+                         ?? UIImage(named: m.icon)) { [weak self] _ in
+        m.closure(self?.argument)
+      })
+      itms[m.group] = subItems
+    }
+    
+    if itms.count == 1, let actions = itms.first?.value as? [UIAction] {
+      return UIMenu(title: "", children: actions )
+    }
+    
+    let menuItems
+    = itms.sorted(by: { $0.key < $1.key })
+      .map {_, itm in UIMenu(title: "",
+                             options: .displayInline,
+                             children:itm) }
+    
+    return UIMenu(title: "",
+                  children:menuItems)
+  }
+  
+  /// Add an additional menu item
+  public func addMenuItem(title: String, icon: String, group: Int? = nil,
+                          closure: @escaping (Any?)->()) {
+    if let group = group {
+      groupedMenu += (title: title, icon: icon, group: group, closure: closure)
+    }
+    else {
+      menu += (title: title, icon: icon, closure: closure)
+    }
   }
   
   // MARK: - UIContextMenuInteractionDelegate protocol
@@ -72,22 +121,31 @@ open class ContextMenu: NSObject, UIContextMenuInteractionDelegate {
     
   @available(iOS 13.0, *)
   public func contextMenuInteraction(_ interaction: UIContextMenuInteraction, previewForHighlightingMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview? {
-      guard self.smoothPreviewForImage == true,
-        let imgV = view as? UIImageView else {
-          //Use Default Menu Appeariance
-          return nil
-      }
-      /// prevent the white background wich is default and appear in some cases as white outline
-      let params = UIPreviewParameters()
-      params.backgroundColor = .black
-      
-      let preview = UIImageView(frame: CGRect(origin: CGPoint.zero,
-                                              size: view.frame.size))
-      preview.image = imgV.image
-      preview.contentMode = imgV.contentMode
-      return UITargetedPreview(view:preview,
-                               parameters: params,
-                               target: UIPreviewTarget(container: view.superview!,
-                                                       center: view.center))
+    
+    var imgV: UIImageView? = view as? UIImageView
+    
+    if imgV == nil {
+      imgV = (view as? NorthUIKit.ImageView)?.imageView
+    }
+    
+    guard self.smoothPreviewForImage == true,
+          let imgV = imgV  else {
+      //Use Default Menu Appeariance
+      return nil
+    }
+    /// prevent the white background wich is default and appear in some cases as white outline
+    let params = UIPreviewParameters()
+    params.backgroundColor = .clear
+
+    
+    let preview = UIImageView(frame: CGRect(origin: CGPoint.zero,
+                                            size: view.frame.size))
+    preview.image = imgV.image
+    preview.tintColor = .white
+    preview.contentMode = imgV.contentMode
+    return UITargetedPreview(view:preview,
+                             parameters: params,
+                             target: UIPreviewTarget(container: view.superview!,
+                                                     center: view.center))
   }
 } // ContextMenu
