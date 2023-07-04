20b3a5aaa878a83dcf8d86da498520cf7aa1512a
Ringo <i17>
2023-07-04 16:12:45 +0200

    revertable helper to find memory leaks

--- src/UIKit/Views/WebView.swift
+++ src/UIKit/Views/WebView.swift
@@ -237,6 +237,9 @@ open class WebView: WKWebView, WKScriptMessageHandler,
   /// Max. number of ongoing errors
   private let maxErrorCount = 5
   
+//  deinit {
+//    log(">>>>> releaseD: \(self)")
+//  }
   
   public func release(){
     self.whenAvailable { _ in }
@@ -245,6 +248,7 @@ open class WebView: WKWebView, WKScriptMessageHandler,
       self.configuration.userContentController.removeScriptMessageHandler(forName: obj.key)
     }
 //    NotificationCenter.default.removeObserver(self)
+//    log(">>>>> releaseOnDisappear: \(self)")
   }
   
   /// The closures to call when content has been loaded
