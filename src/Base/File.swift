ec0af5974d9a398a988dbdea66477f57c0e3b0fa
Ringo <M22>
2024-10-08 14:29:40 +0200

    Fixed Xcode 16 build error

--- src/Base/File.swift
+++ src/Base/File.swift
@@ -214,7 +214,6 @@ open class File: ToString, DoesLog {
     guard hasStat else { throw serror("\(path): No file status available") }
     let old = status!.st_mode
     let new = mode.withCString { s in stat_a2mode(old, s, 0) }
-    guard new != -1 else { throw error("\(mode): Invalid mode string") }
     self.mode = Int64(new)
   }
 
