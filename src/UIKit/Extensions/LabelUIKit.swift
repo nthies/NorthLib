9fde9bb64cd0ee92d2b2d8c69d21d062ab180d65
Ringo <M22>
2025-01-31 09:56:35 +0100

    applied tazze in Toast

--- src/UIKit/Extensions/LabelUIKit.swift
+++ src/UIKit/Extensions/LabelUIKit.swift
@@ -7,6 +7,19 @@
 import UIKit
 
 /**
+ * Helper to access taz Font from taz app target NorthLib
+ * a 2nd font register is not possible!
+ * => this is a kind of a Registry Pattern to solve it
+ * alternative refactor or move all Toast Code to taz App Project
+ */
+public class SharedTazFont {
+  private static let shared = SharedTazFont()
+  private var font: UIFont?
+  static var font: UIFont? { shared.font }
+  public static func register(_ font:UIFont){ shared.font = font }
+}
+
+/**
  * This small extension allows to directly use HTM strings in UILabels
  * via transforming the HTML to an attributed text.
  */
@@ -29,11 +42,18 @@ extension UILabel {
         <span style=\"font-family: '-apple-system', 'HelveticaNeue'; 
          font-size: \(self.font!.pointSize - 2)\">%@</span>
       """, newValue)
-      if let astr = try? NSAttributedString(
+      if let astr = try? NSMutableAttributedString(
         data: str.data(using: .unicode, allowLossyConversion: true)!,
         options: [.documentType: NSAttributedString.DocumentType.html,
                   .characterEncoding: String.Encoding.utf8.rawValue], 
         documentAttributes: nil) {
+        
+        if astr.string.contains("🐾") == true,
+           let tazFont = SharedTazFont.font {
+          let logoRange = (astr.string as NSString).range(of: "🐾")
+          astr.addAttribute(.font, value: tazFont, range: logoRange)
+        }
+        
         self.attributedText = astr
       }
     }
